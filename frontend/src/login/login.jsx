0c046f1a61d0a8046a07ba51e5bfe5be4a394b13
Karishma's Classes <156506602+karishma1900@users.noreply.github.com>
2025-10-11 16:38:52 +0530

    Update login.jsx

--- frontend/src/login/login.jsx
+++ frontend/src/login/login.jsx
@@ -10,7 +10,7 @@ const Login = ({ closeModal, openRegisterModal,onLoginSuccess }) => {
     e.preventDefault();
 
     try {
-      const res = await fetch('https://ai-application-post-create.onrender.com/api/auth/login', {
+      const res = await fetch('https://ai-application-post-create.vercel.app/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         credentials: 'include',
