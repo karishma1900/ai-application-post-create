206867a6894a58270fde880a6b528d9f773990ed
Karishma's Classes <156506602+karishma1900@users.noreply.github.com>
2025-10-13 10:55:18 +0530

    Delete backend directory

--- backend/routes/requestRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { placeRequest } = require('../controllers/requestController');
-const authMiddleware = require('../middlewares/authMiddleware');
-const router = express.Router();
-
-router.post('/place', authMiddleware, placeRequest);
-
-module.exports = router;
